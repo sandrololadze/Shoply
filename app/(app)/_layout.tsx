453915d9f3716b6a9d72f1975d75269dbcac4ac6
Loladze Sandro <sandro.loladze@student.rhizo.be>
2026-05-21 22:20:39 +0200

    fix tabBarLabel styling

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -39,7 +39,7 @@ export default function AppLayout() {
       <Tabs.Screen
         name="groups"
         options={{
-          tabBarLabel: () => <Text style={{ fontSize: 11, fontWeight: '600', color: Colors.primary }}>{t('lists')}</Text>,
+          tabBarLabel: t('lists'),
           tabBarIcon: ({ color, size }) => (
             <Ionicons name="list" color={color} size={size} />
           ),
@@ -55,7 +55,7 @@ export default function AppLayout() {
         name="profile"
         options={{
           title: t('profile'),
-          tabBarLabel: () => <Text style={{ fontSize: 11, fontWeight: '600', color: Colors.textTertiary }}>{t('profile')}</Text>,
+          tabBarLabel: t('profile'),
           tabBarIcon: ({ color, size }) => (
             <Ionicons name="person-circle-outline" color={color} size={size} />
           ),
